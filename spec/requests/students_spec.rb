392379781229876ccd73e722bf767f8365916903
anamaria <anamariamaldonado1994@gmail.com>
2025-08-18 23:23:50 -0400

    adding test for student index

---
+++ spec/requests/students_spec.rb
@@ -0,0 +1,33 @@
+require 'rails_helper'
+
+RSpec.describe "Students", type: :request do
+  let!(:student) do 
+    Student.create(
+      first_name: "KJ",
+      last_name: "Loving",
+      email: "kj@test.com"
+    )
+  end
+
+  let!(:student2) do
+    Student.create(
+      first_name: "AJ",
+      last_name: "Suning",
+      email: "aj@test.com"
+    )
+  end
+
+  describe "GET /students" do
+    it 'returns a page containing all student names' do
+    get '/students'
+   expect(response.body).to include("Students")
+    expect(response.body).to include("First name:")
+    expect(response.body).to include("KJ")
+    expect(response.body).to include("Loving")
+    expect(response.body).to include("AJ")
+    expect(response.body).to include("Suning")
+  end
+end
+end
+
+ 
